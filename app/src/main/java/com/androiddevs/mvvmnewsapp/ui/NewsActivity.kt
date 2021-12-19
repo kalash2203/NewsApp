7d3378138c6b7a5f1fa4b396f48a6c0080896c00
Tanmay <tanmaysingh0311@gmail.com>
2021-12-19 22:17:21 +0530

    Initial commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -0,0 +1,73 @@
+package com.androiddevs.mvvmnewsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
+import com.androiddevs.mvvmnewsapp.repository.NewsRepository
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
+import com.google.android.gms.ads.MobileAds
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import kotlinx.android.synthetic.main.activity_news.*
+
+class NewsActivity : AppCompatActivity() {
+
+    lateinit var viewModel: NewsViewModel
+    private lateinit var remoteConfig: FirebaseRemoteConfig
+   lateinit var mInterstitialAd: InterstitialAd
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+
+
+        MobileAds.initialize(this)
+
+        mInterstitialAd = InterstitialAd(this)
+        mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
+
+
+
+        remoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 0
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+
+
+
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val updated = task.result
+                    Log.d("Value", "Config params updated: $updated")
+
+                } else {
+
+                }
+                val showads: Boolean
+                showads = remoteConfig.getString("show_ads").toBoolean()
+                if (showads){
+                    mInterstitialAd.loadAd(AdRequest.Builder().build())
+                }
+
+            }
+
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(application, newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+
+        //bottom navigation view for navigation between fragments
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+    }
+}
